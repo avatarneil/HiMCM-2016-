7eedccd01b783984ec990ffa7c427433fd4de2dd
Neil Goldader <Neil Goldader>
2016-11-18 22:43:32 -0500

    added updated minimize function

--- HiMCMPython/pythonChecker.py
+++ HiMCMPython/pythonChecker.py
@@ -35,13 +35,14 @@ def minimize() :
    
     for k in range(1,10):
         a=0
-        inc=100*incfact
+        inc=10*incfact
         i=0
         while (i<25):
             print('i =' + str(i))
-        
-            guess2[i] += inc
-        
+            if ((guess2[i]+inc)<=691):
+                guess2[i] += inc
+            print('guess i is ' + str(guess[i]))
+            print('guess2 i is ' + str(guess2[i]))
         
             cost2 = pixelcost(sumfunc(guess2))
             cost = pixelcost(sumfunc(guess))
@@ -49,28 +50,30 @@ def minimize() :
             if (cost2 < cost):
                 print('better')
                 guess[i] = guess2[i]
-                inc=100*incfact
+                inc=10*incfact
                 i+=1
                 a=0
             elif ((cost2 >= cost) and (a==0)):
                 print('worse first time')
-                inc=-inc
+                inc=-2*inc
                 a=1
             elif ((cost2 >= cost) and (a==1)):
                 print('worse not first time')
-                inc=100*incfact
+                inc=10*incfact
                 i+=1
                 a=0
              
-            print('moving on, guess i is ' + str(guess[i]))
-            print('guess2 i is ' + str(guess2[i]))
-             
-        incfact+=1   
+            print('moving on')
+            
+        incfact+=1 
             
     
     
 guess3=[3,86,123,175,173,215,290,219,348,334,563,500,504,522,545,437,484,510,538,546,387,324,356,332,378,398]
-whichpixel=sumfunc(guess3)
+guess4=[3,86,123,175,153,195,270,179,318,334,563,500,504,522,545,437,484,510,508,496,387,254,286,332,378,398]
+print(pixelcost(sumfunc(guess)))
+print(pixelcost(sumfunc(guess3)))
+print(pixelcost(sumfunc(guess4)))
     
 
 for i in range(692):
@@ -86,5 +89,5 @@ for i in range(692):
 
 whichpixel = np.array(whichpixel)
 warehouselist=np.array(warehouselist)
-whichpixel.tofile('whichpixel2.csv',sep=',',format='%10.5f')
+#whichpixel.tofile('whichpixel3.csv',sep=',',format='%10.5f')
 #guesssum.tofile('guesssum.csv',sep=',',format='%10.5f')
